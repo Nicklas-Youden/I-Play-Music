659235e969623a763b15894c0e7721e144f5840a
Nicklas Hansen <nicklasyoudendk@gmail.com>
2021-01-25 00:39:13 +0100

    Welcome done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Router} from "@reach/router"
+import Welcome from './pages/Welcome';
+
 
 function App() {
   return (
     <Router>
-
+      <Welcome path="/"/>
     </Router>
   );
 }
---
+++ src/components/WelcomeLogo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./WelcomeLogo.scss";
+export default function WelcomeLogo(){
+  return(
+    <div className="Welcome__Logo">
+    <img className="WelcomeLogo" src="./images/welcomeLogoLight.png" alt=""/>
+    </div>
+    )
+  }
