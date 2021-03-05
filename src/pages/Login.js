3e61f66e5a7dfe8edc1657d5cb71f868437698bb
Nicklas Hansen <nicklasyoudendk@gmail.com>
2021-03-05 11:13:28 +0100

    make redirect_uri dynamic for prod enviroment

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,12 +7,23 @@ export default function Login() {
   var date = new Date();
   date = date.setSeconds(date.getSeconds() + 10);
   document.cookie = `state=${state}; expires=${date}`;
+
+  var uri;
+
+  if (process.env.NODE_ENV === "production") {
+    uri = "https://iplaymusicnicklashansen.netlify.app/";
+  } else {
+    uri = "http://localhost:8888/callback";
+  }
+
   var options = querystring.stringify({
     response_type: "code",
     client_id: "e734fb09ce11423e8ec459d526ceb050",
     scope:
       "user-read-private user-read-email user-library-read playlist-read-private playlist-read-collaborative",
-    redirect_uri: "http://localhost:8888/callback",
+    redirect_uri: process.env.NODE_ENV === "production" 
+    ? "https://iplaymusicnicklashansen.netlify.app/" 
+    : "http://localhost:8888/callback",
     state,
   });
 
